3308d87da8cad4333201f6eba35d06a9363da8c2
Seth Trowbridge <seth111@gmail.com>
2022-09-26 19:35:31 -0400

    cleanup on Collapse

--- client/Collapse.tsx
+++ client/Collapse.tsx
@@ -3,55 +3,37 @@ import React from "react";
 export const Collapse =({open, instant, className, children}:{open:boolean, instant:boolean, className?:string, children:React.ReactNode})=>
 {
     const [heightGet, heightSet] = React.useState(open ? "auto" : "0px");
-    const [clipGet, clipSet] = React.useState(open ? "visible" : "hidden");
     const [movingGet, movingSet] = React.useState(false);
     const container = React.useRef(null);
-    const style = {
-        height: heightGet,
-        overflow: open ? "visible" : "hidden",
-        transition: "height 1.0s"
-    };
-    console.log(`initial hieght would be ${open ? "auto" : "0px"} instead its ${heightGet} because transition is ${movingGet}`);
+    const style = { height: heightGet, overflow: open ? "visible" : "hidden", transition: "height 1.0s" };
 
-
-    const handleRun =()=>
-    {
-        //console.log("anim start");
-    }
     const handleEnd =()=>
     {
-        //console.log("anim stop");
         movingSet(false);
-        
         if(container.current)
         {
             const el = container.current as Element;
-            //clipSet(open ? "visible" : "hidden");
             el.style.overflow = open ? "visible" : "hidden";
-            
-            if(open)
-            {
-                el.style.height = "auto";
-                //heightSet("auto");
-            }
+            el.style.height = open ? "auto" : "0px";
         }
+    };
 
-    }
-
-    React.useEffect(()=>{
+    React.useEffect(()=>
+    {
         if(container.current)
         {
             const el = container.current as Element;
-            //console.log(el.scrollHeight);
-            el.addEventListener("transitionrun", handleRun);
             el.addEventListener("transitionend", handleEnd);
             
             movingSet(true);
             el.style.overflow = "hidden";
 
-            if(!open)
+            if(open)
             {
-                //console.log("closing")
+                el.style.height = el.scrollHeight+"px";
+            }
+            else
+            {
                 if(!movingGet)
                 {
                     el.style.transition = "none";
@@ -67,20 +49,12 @@ export const Collapse =({open, instant, className, children}:{open:boolean, inst
                     el.style.height = "0px";
                 }
             }
-            else
-            {
-                //console.log("opening");
-                el.style.height = el.scrollHeight+"px";
-            }
 
-            return function()
-            {
-                el.removeEventListener("transitionrun", handleRun);
-                el.removeEventListener("transitionend", handleEnd);
-            };
+            return ()=> el.removeEventListener("transitionend", handleEnd);
         }
 
-    }, [open, children])
+    }, [open, children]);
+    
     return <div ref={container} style={style} className={className}>
         {children}
     </div>
