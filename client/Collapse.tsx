83fc953d5182545ec601ca037dc46c7b1ac390cb
Seth Trowbridge <seth111@gmail.com>
2022-10-01 08:48:59 -0400

    parent adjusting

--- client/Collapse.tsx
+++ client/Collapse.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+type AdjustEvent = "CollapseAdjust"
+type AdjustEventOptions = {bubbles:boolean, detail:number};
+
+const CTX = React.createContext({Adjust:(inAmount:number)=>{console.log(`at root ${inAmount}`)}});
+
 export default ({children, open, instant}:{children:React.ReactNode, open:boolean, instant:boolean})=>
 {
+    const Context = React.useContext(CTX);
     const ref = React.useRef(null as null|HTMLDivElement);
     const [initGet] = React.useState(open?{}:{height:"0px"});
     const [doneGet, doneSet] = React.useState(true);
@@ -48,10 +54,24 @@ export default ({children, open, instant}:{children:React.ReactNode, open:boolea
                     }
                 });
             }
+
+            Context.Adjust(open ? ref.current.scrollHeight : -ref.current.scrollHeight);
         }
     }
     , [open]);
 
+
+    const AdjustActive =(inAmount:number)=>{
+        if(ref.current)
+        {
+            console.log(`Adjust! amount:${inAmount}`);
+            const height = parseInt(ref.current.style.height);
+            ref.current.style.height = `${height + inAmount}px`;
+            Context.Adjust(inAmount);
+        }
+
+    };
+    const AdjustSpoof =(inAmount:number)=>{};
     React.useEffect(()=>
     {
         if(ref.current && doneGet && open)
@@ -61,5 +81,8 @@ export default ({children, open, instant}:{children:React.ReactNode, open:boolea
     }
     , [doneGet]);
 
-    return <div ref={ref} style={initGet} className="bg-red-500 transition-all duration-1000 overflow-hidden box-border">{children}</div>;
+
+    return <div ref={ref} style={{...initGet, transition:"all 3s"}} className="bg-red-500 transition-all duration-1000 overflow-hidden box-border">
+        <CTX.Provider value={{Adjust:doneGet ? AdjustSpoof : AdjustActive}}>{children}</CTX.Provider>
+    </div>;
 };
